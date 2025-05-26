bd1f6e0733613b5fc50116b03cd1f1c9498bcdb3
Duarte Fernandez <duartefernandezpineiro@gmail.com>
2025-05-26 19:54:37 +0200

    P3

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -1,3 +1,15 @@
+#####
+# 
+# This class is part of the Programming the Internet of Things
+# project, and is available via the MIT License, which can be
+# found in the LICENSE file at the top level of this repository.
+# 
+# You may find it more helpful to your design to adjust the
+# functionality, constants and interfaces (if there are any)
+# provided within in order to meet the needs of your specific
+# Programming the Internet of Things project.
+# 
+
 import logging
 
 from time import sleep
@@ -10,31 +22,55 @@ from programmingtheiot.cda.app.DeviceDataManager import DeviceDataManager
 logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
 
 class ConstrainedDeviceApp():
+	"""
+	Definition of the ConstrainedDeviceApp class.
+	
+	"""
+	
 	def __init__(self):
+		"""
+		Initialization of class.
+		
+		@param path The name of the resource to apply to the URI.
+		"""
 		logging.info("Initializing CDA...")
 
 		self.dataMgr = DeviceDataManager()
+		
 
 	def startApp(self):
+		"""
+		Start the CDA. Calls startManager() on the device data manager instance.
+		
+		"""
 		logging.info("Starting CDA...")
-
+		
 		self.dataMgr.startManager()
 
 		logging.info("CDA started.")
 
 	def stopApp(self, code: int):
+		"""
+		Stop the CDA. Calls stopManager() on the device data manager instance.
+		
+		"""
 		logging.info("CDA stopping...")
-
+		
 		self.dataMgr.stopManager()
-
+		
 		logging.info("CDA stopped with exit code %s.", str(code))
 
 def main():
+	"""
+	Main function definition for running client as application.
+	
+	Current implementation runs for 65 seconds then exits.
+	"""
 	cda = ConstrainedDeviceApp()
 	cda.startApp()
-
+	
 	runForever = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.RUN_FOREVER_KEY)
-
+	
 	if runForever:
 		while (True):
 			sleep(5)
@@ -45,4 +81,9 @@ def main():
 		cda.stopApp(0)
 
 if __name__ == '__main__':
+	"""
+	Attribute definition for when invoking as app via command line
+	
+	"""
 	main()
+	
